61458682304e14c031c95122d9a3d67cafd4b702
ErtugUygurlu <ertu_ygrl@hotmail.com>
2023-03-15 11:00:22 +0300

    Add project files.

---
+++ AtKulubu/Controllers/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AtKulubu.Models.Entity;
+
+namespace AtKulubu.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        AtDBEntities db= new AtDBEntities();
+        public ActionResult Index()
+        {
+            var degerler= db.TBLBILGI.ToList();
+            return View(degerler);
+        }
+    }
+}
---
+++ AtKulubu/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AtKulubu.MvcApplication" Language="C#" %>
